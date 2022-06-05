2af65501de82c815c9005703eede71b301ade508
9tin9tin9 <9tin9tin9@gmail.com>
2022-06-05 21:00:42 +0800

    Revert "small changes to foreach_range and foreach_untilNull"
    This reverts commit 439403d4ee4b9674e34f3f9db52744aeb99565ec.

--- utility/for.h
+++ utility/for.h
@@ -2,7 +2,7 @@
 #define _FOR_H_
 
 #define foreach_range(var, start, end) \
-    for (__typeof__(*(start))* var = (start); var < (end); var++)
+    for (__typeof__(start) var = (start); var < (end); var++)
 
 #define foreach_iter(type, container, var) \
     for (type##_iter(__##type##_type(container)) var = type##_begin(container); \
@@ -11,6 +11,6 @@
 
 // Only use it when you are sure the array ends with NULL
 #define foreach_untilNull(var, start) \
-    for (__typeof__(*(start))* var; *var != NULL; var++)
+    for (__typeof__(start) var; *var != NULL; var++)
 
 #endif
