aa32ea62e85136628d0bcef31916a8189bdbd752
9tin9tin9 <9tin9tin9@gmail.com>
2022-06-07 14:27:32 +0800

    Revert "small changes to foreach_untilNull"
    This reverts commit cc3e67a69dca7c537c4c8890bc478cab140f3dd0.

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-#include "utility/opts.h"
+#include "container/hashmap.h"
+#include "container/vec.h"
+#include "utility/print.h"
+#include "utility/for.h"
 
 int main(){
+    HMap(int) a = HMap_new(int);
+    HMap_insert(a, "first", 10);
+    HMap_at(a, "first") = 20;
+    HMap_insert(a, "first", 1);
+    HMap_insert(a, "second", 2);
+    HMap_insert(a, "third", 3);
+
+    foreach_iter(HMap, a, entry){
+        printsp(**entry);
+    }
+
+    Vec(int) b = Vec_new(int, 1, 2, 3, 4);
+    foreach_iter(Vec, b, it){
+        printsp(*it);
+    }
 }
